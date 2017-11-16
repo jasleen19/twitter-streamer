9e84b275fa6ec382c2361b9d13847f7ed297ffb4
Jasleen Kaur <jasleenkaur1909@gmail.com>
2017-11-16 12:12:02 -0500

    Fixed bugs and improved code

--- streamer.py
+++ streamer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# imports
+# Imports
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -8,36 +8,62 @@ from threading import Timer
 from http.client import IncompleteRead
 from pprint import pprint
 import os
-# from kafka import SimpleProducer, KafkaClient
 
+# StreamListener class to process Twitter stream using Tweepy Stream object
 class StreamListener(StreamListener):
-    '''StreamListener class: This class works as a handler for the incoming stream'''
+    '''StreamListener class: This class works as a handler for the incoming
+    stream'''
     def on_data(self, data):
-        '''on_data class_method: handle data by loading into a json and appending to global repository of tweets,
+        '''on_data class_method: handle data by loading into a json and
+        appending to global repository of tweets,
         based on minute as index'''
-        global tweets, i
-        # producer.send_messages("streamer", data.encode('utf-8'))
+        global users, i
         tweet_json = loads(data)
         if 'user' in tweet_json:
-            tweets[i-1].append({'user': tweet_json['user']['screen_name'], 'user_statuses': tweet_json['user']['statuses_count'] })
+            username = tweet_json['user']['screen_name']
+            # increase count or add user to key and then increase count
+            users[username] = users.get(username, 0) + 1
+            # # debug print
+            # if users[username] == 1:
+            #     print("Minute {}: {} tweeted!".format(i, username))
+            # else:
+            #     print("Minute {}: {} tweeted AGAIN! Count = {}".format(i,
+            #     username, users[username]))
         return True
 
     def on_error(self, status):
-        '''on_error class_method: handle the errors are most frequently on server side/connection related and few, pass them'''
+        '''on_error class_method: handle the errors are most frequently on
+        server side/connection related and few, pass them'''
         pass
 
 def printer():
-    '''printer function: run a self-calling throad to be called every 60 seconds on the clock (not equivalent to sleep!)'''
-    global tweets, i, keyword
-    tweets.append([]) # append (current_minute + 1)s list
+    '''printer function: run a self-calling throad to be called every 60
+    seconds on the clock (not equivalent to sleep!)'''
+    global users, i, keyword, duration
     i = i + 1
-    Timer(60.0, printer).start() # called every minute
+    # called every minute; change value to 10 seconds to test faster
+    Timer(duration, printer).start()
     if i != 1:
-        print("\nUsers who tweeted about {} in last 1 minute:".format(keyword))
-        pprint(sum(tweets[-6:-1], []))
+        print("\nUsers who tweeted about {} in last {} seconds:".format(keyword,
+         duration))
+        pprint(users)
+        print("\n")
+    # empty dictionary every 5 minutes
+    if i % duration == 0:
+        users = {}
+
+# GLOBAL VARIABLES DECLARED AND INITIALIZED - need to remove the need to have
+#   global variables
+users = {}
+i = 0
+keyword = ""
+# change this parameter to make the printing/storing faster or slower. Default
+#   is 60 seconds
+duration = 60.0
 
 def main():
-    '''main function: initialize tokens, validate them, run printer thread and start streaming'''
+    '''main function: initialize tokens, validate them, run printer thread and
+     start streaming'''
     # token information, intialize this if empty or use runtime input
     access_token = ""
     access_token_secret = ""
@@ -45,8 +71,10 @@ def main():
     consumer_secret = ""
 
     # CHECK CREDENTIALS
-    if consumer_key == "" or consumer_secret == "" or access_token == "" or access_token_secret == "":
-        print("Some values were not defined in the program! Create them at https://apps.twitter.com/")
+    if consumer_key == "" or consumer_secret == "" or access_token == "" or \
+        access_token_secret == "":
+        print("Some values were not defined in the program! Create them at \
+            https://apps.twitter.com/")
     if consumer_key == "":
         print("Enter consumer key:")
         consumer_key = input()
@@ -62,21 +90,17 @@ def main():
 
     # TAKE INPUT
     print("Enter your keyword:")
+    global keyword
     keyword = input()
 
     # COPIED
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    # kafka = KafkaClient("localhost:9092")
-    # producer = SimpleProducer(kafka)
-
-    # GLOBAL VARIABLES DECLARED AND INITIALIZED - need to remove the need to have global variables
-    tweets = [[]]
-    i = 0
 
     # START PRINTING
     print("\nConsuming the stream...")
-    printer() # THIS WILL BECOME AN INDEPENDENT PROCESS
+    # THIS WILL BECOME AN INDEPENDENT PROCESS
+    printer()
 
     l = StreamListener()
 
@@ -84,11 +108,13 @@ def main():
     while True:
         try:
             stream = Stream(auth, l)
-            stream.filter(track=[keyword]) # THIS FUNCTION IS RUNNING AS `MAIN` PROCESS
+            # THIS FUNCTION IS RUNNING AS `MAIN` PROCESS
+            stream.filter(track=[keyword])
         except (Exception, IncompleteRead) as e:
             continue
+        # kill kernel - need to address this
         except KeyboardInterrupt:
-            os._exit(1) # kill kernel - need to address this
+            os._exit(1)
 
 if __name__ == '__main__':
     main()
