50cb8c85b9d9d549bf30825b0f221036c59e6551
Jasleen Kaur <jasleenkaur1909@gmail.com>
2017-11-17 08:47:43 -0500

    Fixed printing

--- streamer.py
+++ streamer.py
@@ -11,30 +11,29 @@ from collections import OrderedDict
 from itertools import islice
 import os
 
-# StreamListener class to process Twitter stream using Tweepy Stream object
 class StreamListener(StreamListener):
     '''StreamListener class: This class works as a handler for the incoming
-    stream'''
+    Twitter stream using Tweepy object'''
     def on_data(self, data):
         '''on_data class_method: handle data by loading into a json and
         appending to global repository of tweets,
         based on minute as index'''
-        global i, minutes
+        global i, durations
         tweet_json = loads(data)
         if 'user' in tweet_json:
             username = tweet_json['user']['screen_name']
-            # if minute not in `minutes` dictionary, add it
-            if i not in minutes:
-                minutes[i] = {}
+            # if minute not in `durations` dictionary, add it
+            if i not in durations:
+                durations[i] = {}
             # check if username is being tracked in current minute and
             #   increase count
-            minutes[i][username] = minutes[i].get(username, 0) + 1
+            durations[i][username] = durations[i].get(username, 0) + 1
             # debug print
-            # if minutes[i][username] == 1:
+            # if durations[i][username] == 1:
             #     print("Minute {}: {} tweeted!".format(i, username))
             # else:
             #     print("Minute {}: {} tweeted AGAIN! Count = {}".format(i,
-            #     username, minutes[i][username]))
+            #     username, durations[i][username]))
         return True
     
     def on_error(self, status):
@@ -48,7 +47,7 @@ class Printer():
     def printer(self):
         '''printer function: run a self-calling throad to be called every 
         60 seconds on the clock (not equivalent to sleep!)'''
-        global i, keyword, duration, minutes, units
+        global i, keyword, duration, durations, units
         i = i + 1
         # called every minute; change to 10 seconds to test faster
         Timer(duration, self.printer).start()
@@ -56,24 +55,23 @@ class Printer():
         #   which has not finished yet
         if i != 1:
             # create an ordered dict so that slicing is possible
-            ordered = dict(OrderedDict(islice(minutes.items(), 
+            ordered = dict(OrderedDict(islice(durations.items(), 
                 max(0,i-units-1), i-1)))
             merged = {}
             # merge all the dictionaries of counts for last `units * duration` 
             #   seconds
             for j, (k,v) in enumerate(ordered.items()):
                 merged = {**merged, **v}
-            print("\nMinute {}: Tweeters about {} in last {} seconds:".format(i-1, 
-                keyword, int(units * duration)))
+            print("\nDuration unit {}: Tweeters about {} in last {} seconds:"
+                .format(i-1, keyword, int(min((i-1) * duration, units * duration))))
             print(tabulate([(k, v) for k,v in merged.items()] , 
                 headers=['Username', 'Count of Tweets']))
 
-# GLOBAL VARIABLES DECLARED AND INITIALIZED - need to remove the need to have
-#   global variables
-minutes = {}
+# GLOBAL VARIABLES DECLARED AND INITIALIZED
+durations = {}
 i = 0
 # change this to allow printing frequency (float: in seconds)
-duration = 60.0
+duration = 10.0
 # duration units to go back to
 units = 5
 keyword = ""
@@ -90,24 +88,24 @@ def main():
     # CHECK CREDENTIALS
     if consumer_key == "" or consumer_secret == "" or access_token == "" or \
         access_token_secret == "":
-        print("Some values were not defined in the program! Create them at \
-            https://apps.twitter.com/")
+        print("Some values were not defined in the program!")
+        print("Create them at https://apps.twitter.com/")
     if consumer_key == "":
         print("Enter consumer key:")
-        consumer_key = input()
+        consumer_key = input().strip()
     if consumer_secret == "":
         print("Enter consumer secret:")
-        consumer_secret = input()
+        consumer_secret = input().strip()
     if access_token == "":
         print("Enter access token:")
-        access_token = input()
+        access_token = input().strip()
     if access_token_secret == "":
         print("Enter access token secret:")
-        access_token_secret = input()
+        access_token_secret = input().strip()
 
     # TAKE INPUT
     print("Enter your keyword:")
-    global keyword
+    global keyword, duration, units
     keyword = input()
 
     # COPIED
@@ -115,7 +113,12 @@ def main():
     auth.set_access_token(access_token, access_token_secret)
 
     # START PRINTING
+    print("\nDuration: {}".format(duration))
+    print("periodic time (seconds) after which printing is done")
+    print("\nDuration Units: {}".format(units))
+    print("time units to look back to, each of length `Duration`")
     print("\nConsuming the stream...")
+    print("`ctrl/cmd + c` twice to exit cleanly")
     # THIS WILL BECOME AN INDEPENDENT PROCESS
     p = Printer()
     p.printer()
